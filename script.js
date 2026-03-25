772218c409613b7903b4985c7069c142805878f3
Katherine Vasquez <kathevasquez@gmail.com>
2026-03-25 17:51:40 -0500

    feat: generar colores en formato HSL y HEX

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const generateBtn = document.getElementById("generateBtn");
 const paletteSize = document.getElementById("paletteSize");
 
 // Función para generar un color HSL aleatorio
-
 function generarColorHSL() {
     
     const h = Math.floor(Math.random() * 360);
@@ -13,16 +12,30 @@ function generarColorHSL() {
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
+// Función para generar un color HEX aleatorio
+function generarColorHEX() {
+
+    const letras = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letras[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
 // Evento click del botón
 generateBtn.addEventListener("click", function () {
 
     // Obtener el valor del select
     const cantidad = paletteSize.value;
 
-    const color = generarColorHSL();
+    const colorHSL = generarColorHSL();
+    const colorHEX = generarColorHEX();
 
     // Mostrar en consola
     console.log("Cantidad seleccionada:", cantidad);
-     console.log("Color generado:", color);
-
+    console.log("Color HSL:", colorHSL);
+    console.log("Color HEX:", colorHEX);
 });
