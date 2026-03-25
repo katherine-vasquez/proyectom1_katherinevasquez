5ab13acff045bc197d34c01c9265bfbab23fea64
Katherine Vasquez <kathevasquez@gmail.com>
2026-03-25 17:39:39 -0500

    feat: generar color aleatorio en formato HSL

--- script.js
+++ script.js
@@ -2,13 +2,27 @@
 const generateBtn = document.getElementById("generateBtn");
 const paletteSize = document.getElementById("paletteSize");
 
+// Función para generar un color HSL aleatorio
+
+function generarColorHSL() {
+    
+    const h = Math.floor(Math.random() * 360);
+    const s = Math.floor(Math.random() * 100);
+    const l = Math.floor(Math.random() * 100);
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
 // Evento click del botón
 generateBtn.addEventListener("click", function () {
 
     // Obtener el valor del select
     const cantidad = paletteSize.value;
 
+    const color = generarColorHSL();
+
     // Mostrar en consola
     console.log("Cantidad seleccionada:", cantidad);
+     console.log("Color generado:", color);
 
 });
